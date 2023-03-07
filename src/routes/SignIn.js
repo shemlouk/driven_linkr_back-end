38b77525f230e44bf57ed0d04f21bb9c9c4a2af7
samuel <shemlouk@gmail.com>
2023-03-07 01:29:30 -0300

    feat: add /signin DELETE endpoint

--- src/routes/SignIn.js
+++ src/routes/SignIn.js
@@ -1,9 +1,11 @@
 import controller from "../controllers/SessionsController.js";
+import authentication from "../middlewares/authentication.js";
 import validateBody from "../middlewares/validateBody.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/signin", validateBody, controller.create);
+router.delete("/signin", authentication, controller.delete);
 
 export default router;
