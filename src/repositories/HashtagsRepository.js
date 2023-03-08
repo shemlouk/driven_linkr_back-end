285107a1563bc883a787f4a0bccd52aac0a56914
Mikael Josias <mikaeljosias2002@gmail.com>
2023-03-07 23:06:18 -0300

    feat(HashtagsRepository): add get posts with hastag id function

--- src/repositories/HashtagsRepository.js
+++ src/repositories/HashtagsRepository.js
@@ -5,6 +5,12 @@ class HashtagsRepository {
         const res = db.query(`SELECT * FROM hashtags ORDER BY quantity DESC LIMIT 10;`);
         return res;
     }
+    async getPostsWithHashtagId(hashtagId) {
+        const res = db.query(
+            `SELECT p.*, h.name FROM posts p JOIN posts_hashtags ph ON p.id = ph.post_id JOIN hashtags h ON h.id = ph.hashtag_id WHERE ph.hashtag_id = $1 ORDER BY created_at DESC;`,
+            [hashtagId]);
+        return res;
+    }
 }
 
 export default new HashtagsRepository();
