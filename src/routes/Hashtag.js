d97b7a9e550f1918f11dd015524ac2230831fd0d
Mikael Josias <mikaeljosias2002@gmail.com>
2023-03-07 23:08:42 -0300

    feat(Hashtag): add route to get posts with hashtag id

--- src/routes/Hashtag.js
+++ src/routes/Hashtag.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import HashtagsController from "../controllers/HashtagsController.js";
 import authentication from "../middlewares/authentication.js";
+import validateBody from "../middlewares/validateBody.js";
 
 const router = Router();
 
 router.get("/trending", authentication, HashtagsController.getTrending);
+router.get("/hashtag/:id", authentication, HashtagsController.getPostsWithHashtagId);
 
 export default router;
