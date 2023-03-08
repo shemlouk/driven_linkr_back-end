78600989ca66c0677358e06c3fda05b34583d6ac
Mikael Josias <mikaeljosias2002@gmail.com>
2023-03-08 00:04:44 -0300

    feat: add create and update hashtag

--- src/repositories/HashtagsRepository.js
+++ src/repositories/HashtagsRepository.js
@@ -1,7 +1,23 @@
 import db from "../config/database.js";
 
 class HashtagsRepository {
-    async getTrending () {
+    async create(hashtagName) {
+        const res = db.query(
+            `INSERT INTO hashtags (name, quantity) VALUES ($1, 1) RETURNING id;`,
+            [hashtagName]);
+        return res;
+    }
+    async updateQuantityByOne(quantity, id) {
+        const res = db.query(`UPDATE hashtags SET quantity = $1 WHERE id = $2 RETURNING id;`, [(quantity + 1), id]);
+        return res;
+    }
+    async getHashtagByName(hashtagName) {
+        const res = db.query(
+            `SELECT * FROM hashtags WHERE name = $1;`,
+            [hashtagName]);
+        return res;
+    }
+    async getTrending() {
         const res = db.query(`SELECT * FROM hashtags ORDER BY quantity DESC LIMIT 10;`);
         return res;
     }
