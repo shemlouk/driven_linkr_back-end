5443273e208e98c545ee6954cc7f8d23fb806b7d
DFCruzz <diegofcruzz@gmail.com>
2023-03-09 15:08:17 -0300

    refactor: changing getPostList to include username and picture

--- src/repositories/UsersRepository.js
+++ src/repositories/UsersRepository.js
@@ -24,9 +24,13 @@ class UsersRepository {
   }
   async getPostList() {
     const res = await db.query(
-      `SELECT * FROM posts ORDER BY created_at DESC LIMIT 20;`
+      `
+        SELECT posts.* FROM posts
+        JOIN users ON users.id = posts.user_id 
+        ORDER BY created_at DESC LIMIT 20;
+      `
     );
-    return res;
+    return res
   }
   async insertPost(description, url, previewTitle, previewDesc, previewImg, userId) {
     const res = await db.query(
