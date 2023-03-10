168b5646c6b96f239ef38aded129f22beb0178c1
DFCruzz <diegofcruzz@gmail.com>
2023-03-10 03:41:02 -0300

    refactor: return post insertion

--- src/repositories/UsersRepository.js
+++ src/repositories/UsersRepository.js
@@ -41,7 +41,7 @@ class UsersRepository {
     userId
   ) {
     const res = await db.query(
-      `INSERT INTO posts (description, url, preview_title, preview_desc, preview_img, user_id) VALUES ($1, $2, $3, $4, $5, $6);`,
+      `INSERT INTO posts (description, url, preview_title, preview_desc, preview_img, user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`,
       [description, url, previewTitle, previewDesc, previewImg, userId]
     );
     return res;
