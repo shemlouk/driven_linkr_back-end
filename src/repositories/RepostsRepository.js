6497c913268a234b3cbd88103f704ed5bb2a9ed7
samuel <shemlouk@gmail.com>
2023-03-26 16:00:34 -0300

    fix: change sql query for reposts

--- src/repositories/RepostsRepository.js
+++ src/repositories/RepostsRepository.js
@@ -22,21 +22,11 @@ class RepostsRepository {
     );
     return res;
   }
-  async getReposts(offset, network) {
-    const { rowCount } = await db.query(`SELECT * FROM posts_reposts `);
-    // Se offset >= a contagem total, retorna um array vazio
-    const total = rowCount;
-    if (offset >= total) {
-      return {rows: []};
-    }
-    //verifica qual é o menor valor 10 ou o restante de posts da contagem
-    //se 10 for menor, limita a 10 a quantidade de posts
-    //Se limit for menor, retorna somente a quantidade de posts restantes
-    //impedindo que retorna posts repetidos
-    const limit = Math.min(10, total - offset);
+  async getReposts(timestamp, network) {
     const res = await db.query(
       `
       SELECT  COUNT(posts_reposts.post_id) AS "num_reposts",
+              posts_reposts.created_at AS "rb_created_at",
               posts.*,
               users.name,
               users.profile_picture,
@@ -63,27 +53,26 @@ class RepostsRepository {
         ) AS comments ON posts.id = comments.post_id
         LEFT JOIN (
           SELECT	users.id AS "rb_user_id",
-                  users.name AS "rb_user_name",
-                  posts_reposts.created_at AS "rb_created_at"
+                  users.name AS "rb_user_name"
           FROM posts_reposts
           JOIN users ON posts_reposts.user_id = users.id
         ) AS reposted_by ON posts_reposts.user_id = reposted_by.rb_user_id
       WHERE posts.deleted_at IS NULL
-            AND posts.user_id = ANY ($3::int[])
+            AND posts_reposts.user_id = ANY ($2::int[])
+            AND posts_reposts.created_at <= $1
       GROUP BY	posts_reposts.user_id,
-            posts.id,
-            users.name,
-            users.profile_picture,
-            likes.likes_count,
-            likes.likes_names,
-            comments.num_comments,
-            reposted_by.rb_user_id,
-            reposted_by.rb_user_name,
-            reposted_by.rb_created_at
+                posts.id,
+                users.name,
+                users.profile_picture,
+                likes.likes_count,
+                likes.likes_names,
+                comments.num_comments,
+                reposted_by.rb_user_id,
+                reposted_by.rb_user_name,
+                posts_reposts.created_at
       ORDER BY posts.created_at DESC
-      LIMIT $1 OFFSET $2
       `,
-      [limit, offset, network]
+      [timestamp, network]
     );
     return res;
   }
